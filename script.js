b767f3c8047125201495302df5de89e60b45c97d
Zholdybai <55502117+Zholdybai@users.noreply.github.com>
2019-09-18 19:27:51 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function soonclick(){
+  document.getElementById("kp1").style.display="none";
+  document.getElementById("kp2").style.display="block";
+  document.getElementById("kp4").style.display="none";
+}
+function scheduleclick(){
+  document.getElementById("kp1").style.display="block";
+  document.getElementById("kp2").style.display="none";
+  document.getElementById("kp4").style.display="none";
+}
+function kinoclick(){
+  document.getElementById("kp1").style.display="none";
+  document.getElementById("kp2").style.display="none";
+  document.getElementById("kp4").style.display="block";
+}
